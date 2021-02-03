fd71f8618624e71490ad9fbca81c850b777edf88
Peter McDonough <mcdonop@gmail.com>
2021-02-02 22:47:50 -0500

    cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {MatCardModule} from '@angular/material/card';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCardModule,DragDropModule ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule,DragDropModule,MatIconModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
